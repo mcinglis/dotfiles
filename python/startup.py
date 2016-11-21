d809482fbe998217097f609dc98492e79b14e077
Malcolm Inglis <me@minglis.id.au>
2016-11-21 00:50:58 -0800

    Simplify python startup

--- python/startup.py
+++ python/startup.py
@@ -1,33 +1,18 @@
 
-# These modules will be available in interactive sessions.
-import os
-import sys
-import string
-import random
+# These modules and functions will be available in interactive sessions.
+
 import datetime
 import json
+import os
 from pprint import pprint
+import random
+import sys
+import string
+import subprocess
 
-history_path = os.path.expanduser('~/.python/history')
 
-try:
-    import readline, rlcompleter
-    readline.parse_and_bind('tab: complete')
-
-    def save_history(history_path=history_path):
-        import readline
-        readline.write_history_file(history_path)
-
-    import atexit
-    atexit.register(save_history)
-
-    if os.path.exists(history_path):
-        readline.read_history_file(history_path)
-
-    del readline, rlcompleter, save_history, atexit
-
-except ImportError:
-    pass
-
-del history_path
+def run(command, **kwargs):
+    """Takes a string and executes it as a Bash command."""
+    kwargs.setdefault('timeout', 60)
+    return subprocess.check_call(['bash', '-c', command], **kwargs)
 
